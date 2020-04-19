17842fa7fdae5248e5f5ca90db71da1c81b46b5f
felipe_allmeida_ <almeidafelipewanted@gmail.com>
2020-04-19 12:05:31 -0300

    Fixed security breach

--- MessagesService/Program.cs
+++ MessagesService/Program.cs
@@ -19,9 +19,9 @@ namespace MessagesService
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args)                
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
+                {                    
                     webBuilder.UseStartup<Startup>();
                 });
     }
